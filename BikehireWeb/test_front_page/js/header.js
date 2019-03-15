9e0b63fc96c1f3cae78e81a4e40ee38290517d6f
sc18ql <sc18ql@leeds.ac.uk>
2019-03-15 23:49:08 +0000

    front login and index pages

---
+++ BikehireWeb/test_front_page/js/header.js
@@ -0,0 +1,10 @@
+
+$(function() {
+    $('input[name="daterange"]').daterangepicker({
+        opens: 'left',
+        minDate:new Date()
+    }, function(start, end, label) {
+        console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+
+    });
+});
